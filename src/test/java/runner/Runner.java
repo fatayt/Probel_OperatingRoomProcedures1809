bc0ce2790a7f464b42cf824b5ea5a91cbe4c9251
fatayt <114427129+fatayt@users.noreply.github.com>
2023-09-13 07:37:44 +0300

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources",
         glue = "stepdefinitions",
-        tags = "@Smoke",
+        tags = "@TC0068",
         dryRun = false
 
 )
